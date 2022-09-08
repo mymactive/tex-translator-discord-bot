74a3f4b419126713d8f66b48a905540d0c8baa11
MiyataYuya <katorisenkouhanabi@gmail.com>
2022-09-08 20:44:08 +0900

    コメントを追加

--- latex_src/tut.py
+++ latex_src/tut.py
@@ -7,7 +7,11 @@ import os
 import subprocess
 import numpy as np
 
-# コマンドライン引数を受け取る
+# このプログラムを実行するには、discordボットのトークンが必要。これはgithubにアップできないので、どうするか考え中。このトークンを記述したテキストファイル（例えば token.txt）をローカルのどこかに配置する。
+# プログラムを実行するときは /latex_src ディレクトリから python tut.py token.txt のように実行する。
+
+
+# コマンドライン引数を受け取る。
 args = sys.argv
 
 
@@ -20,27 +24,29 @@ class MyClient(discord.Client):
         print(f'Message from {message.author}: {message.content}')
         # もし'$'から始まるメッセージを受けとったら
         if message.content.startswith('$'):
-            # await message.channel.send('hello')
             # ./latex_src/main.texファイルを書込みモードで開く。
             with open('main.tex', 'w') as f:
                 # メッセージの内容を./latex_src/main.texに書き込む。
                 f.write(message.content)
-            # execute.shを実行することでtexをコンパイルする。
-            # subprocess.Popen('sh execute.sh', shell=True).communicate()
             subprocess.Popen('tex2img structure.tex fig.png',
                              shell=True).communicate()
-            # 生成したものが'fig.pdf'として出力されるので、これを読み込み、sendする。
+            # 生成したものが'fig.png'として出力されるので、これを読み込み、sendする。
             with open('fig.png', 'rb') as g:
                 pic = discord.File(g)
                 await message.channel.send(file=pic)
 
 
+# この部分はdiscord.pyのチュートリアルのまま。仕組みは謎。
 intents = discord.Intents.default()
 intents.message_content = True
 
+# おそらくここで、インスタンスを生成している。
 client = MyClient(intents=intents)
 
+# コマンドラインで受け取ったファイルを読み、tokenに格納する。
 token = 'mytoken'
 with open(args[1]) as f:
     token = f.readline()
+
+# ボットを起動
 client.run(token)
